ef398da010f3398e463ca9d03f82fa8d3159c8ca
shoyonp <shoyonkumar143@gmail.com>
2024-10-01 18:42:34 +0600

    explore done

---
+++ js/comments.js
@@ -0,0 +1,16 @@
+const loadComments = () => {
+  fetch("https://jsonplaceholder.typicode.com/comments")
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error("error happend", error));
+};
+
+const loadComments2 = async () => {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/comments");
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log("data load error");
+  }
+};
---
+++ js/post.js
@@ -0,0 +1,29 @@
+function loadPosts() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) => displayPosts(data));
+}
+
+/*
+1. get the container where you want to add the new element.
+2. creat child element\
+3. set innerText or innetHTML
+4. appendChild
+*/
+
+function displayPosts(posts) {
+  const postsContainer = document.getElementById("posts-container");
+  for (const post of posts) {
+    const postDiv = document.createElement("div");
+    postDiv.classList.add("post");
+    postDiv.innerHTML = `
+        <h4>User-${post.userId}</h4>
+        <h5>Post: ${post.title}</h5>
+        <p>Post Description: ${post.body}</p>
+        `;
+    postsContainer.appendChild(postDiv);
+  }
+}
+
+loadPosts();
+
